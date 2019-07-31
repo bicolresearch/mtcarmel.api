256546b1d04474ffc168dd4cabb0c959274f1f12
JOAN <joan.gregorio79@gmail.com>
2019-07-31 11:38:38 +0800

    added new controller and model for city, province and country

---
+++ application/controllers/City.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+    Filename    : City.php
+    Location    : application/controllers/City.php
+    Purpose     : City controller
+    Created     : 07/31/2019 11:08:56 by Scarlet Witch
+    Updated     : 
+    Changes     : 
+*/
+
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+use Restserver\Libraries\REST_Controller;
+
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+
+class City extends REST_Controller
+{
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+    }
+
+    public function index_get()
+    {
+        // Get the data from the database
+        $getAll = $this->selection_references_model->_get_all_city();
+
+        // Get the id parameter
+        $id = (int)$this->get('id');
+
+        // Validate id
+        if (empty($id)) {
+            // In case the database result returns NULL
+            if (empty($getAll)) {
+                // Set the response and exit
+                $this->response([
+                    'status' => FALSE,
+                    'message' => 'Not Found'
+                ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+            } else {
+                // Set the response and exit
+                $this->response($getAll, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+            }
+        } else {
+            // Set the response and exit
+            $this->response([
+                'status' => FALSE,
+                'message' => 'Bad Request'
+            ], REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
+        }
+    }
+
+
+
+    
+}
