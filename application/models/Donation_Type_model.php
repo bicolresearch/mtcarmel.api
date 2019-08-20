7474eba7588ed45d411de4b9b09bcb3c737090cc
JOAN <joan.gregorio79@gmail.com>
2019-08-20 12:24:00 +0800

    changed table for all dropdown type into 1 table (dropdown_references) - from donation_schedule_type to dropdown_references

--- application/models/Donation_Type_model.php
+++ application/models/Donation_Type_model.php
@@ -5,8 +5,8 @@
     Location    : application/models/Donation_type_model.php
     Purpose     : Donation Type model
     Created     : 2019-07-01 15:57:35 by Scarlet Witch
-    Updated     : 2019-07-02 19:24:48 by Scarlet Witch 
-    Changes     : update script, check the datenow if same month as scheduled
+    Updated     : 08/17/2019 17:45:04 by Scarlet Witch
+    Changes     : changed table for all dropdown type into 1 table (dropdown_references) - from donation_schedule_type to dropdown_references 
 */
 
 
@@ -34,7 +34,7 @@ class Donation_type_model extends CI_Model
                 't4.full_path as cover_photo')  
             ->from('donation_type AS t1')
             ->join('branch AS t2', 't2.id = t1.branch_id', 'left')
-            ->join('donation_schedule_type AS t3', 't3.id = t1.donation_schedule_type_id', 'left') 
+            ->join('dropdown_references AS t3', 't3.id = t1.donation_schedule_type_id', 'left') 
             ->join('media AS t4', 't4.id = t1.media_id', 'left')               
             ->where_in('t3.name', array('Always', date('F')))   
             ->where('t1.is_deleted', 0);
@@ -56,7 +56,7 @@ class Donation_type_model extends CI_Model
                 't4.full_path as cover_photo')  
             ->from('donation_type AS t1')
             ->join('branch AS t2', 't2.id = t1.branch_id', 'left')
-            ->join('donation_schedule_type AS t3', 't3.id = t1.donation_schedule_type_id', 'left') 
+            ->join('dropdown_references AS t3', 't3.id = t1.donation_schedule_type_id', 'left') 
             ->join('media AS t4', 't4.id = t1.media_id', 'left')               
             ->where_in('t3.name', array('Always', date('F')))                  
             ->where('t1.is_deleted', 0)            
