18d446a52a40e6a701590b6eb5e2b25db4a82eb4
JOAN <joan.gregorio79@gmail.com>
2019-10-07 15:07:46 +0800

    added new controller - service application form

---
+++ application/controllers/Service_application_forms.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+    Filename    : Service_application_forms.php
+    Location    : application/controllers/Service_application_forms.php
+    Purpose     : Service application forms controller
+    Created     : 10/03/2019 19:22:47 by Scarlet Witch
+    Updated     : 
+    Changes     : 
+*/
+
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+use Restserver\Libraries\REST_Controller;
+
+/** @noinspection PhpIncludeInspection */
+require APPPATH . 'libraries/REST_Controller.php';
+/** @noinspection PhpIncludeInspection */
+require APPPATH . 'libraries/Format.php';
+
+class Service_application_forms extends REST_Controller
+{
+    private $sub_module_id;
+
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+        $this->sub_module_id = 18;
+    }
+
+    public function index_get()
+    {               
+        $branch_id = (int)$this->get('branch_id'); 
+
+        $sub_module = $this->service_references_model->_get_sub_modules_by_id($branch_id, $this->sub_module_id);
+
+        $service_application_forms = [            
+            'sub_module' =>  $this->service_references_model->_get_sub_modules_by_id($branch_id, $this->sub_module_id),
+            'form_fields' => $this->service_references_model->_get_all_application_forms($branch_id)     
+        ];    
+
+        if (empty($branch_id)) {
+            // Invalid id, set the response and exit.
+            $this->response([
+                'status' => FALSE,
+                'message' => 'Bad Request'
+            ], REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
+        } else{
+            if ($sub_module === FALSE) {
+                $this->response([
+                    'status' => FALSE,
+                    'message' => 'Not Found'
+                ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+            }   
+            else {
+                if (empty($service_application_forms)) {
+                    // Set the response and exit
+                    $this->response([
+                        'status' => FALSE,
+                        'message' => 'Not Found'
+                    ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+                } else {
+                    // Set the response and exit
+                    $this->response($service_application_forms,REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+                }
+            }
+        } 
+        
+    }
+
+    
+}
