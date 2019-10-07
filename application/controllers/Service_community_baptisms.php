28634af75f44539411c93b307567131b4fdafb2c
JOAN <joan.gregorio79@gmail.com>
2019-10-07 15:09:26 +0800

    updated controllers for service baptism (individual, community and adult)

---
+++ application/controllers/Service_community_baptisms.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+    Filename    : Service_community_baptisms.php
+    Location    : application/controllers/Service_community_baptisms.php
+    Purpose     : Service community baptisms controller
+    Created     : 07/29/2019 14:51:18 by Scarlet Witch
+    Updated     : 10/06/2019 14:24:50 by Scarlet Witch
+    Changes     : renamed forms, added branch id, removed id, added sub_module_id
+*/
+
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+use Restserver\Libraries\REST_Controller;
+
+/** @noinspection PhpIncludeInspection */
+require APPPATH . 'libraries/REST_Controller.php';
+/** @noinspection PhpIncludeInspection */
+require APPPATH . 'libraries/Format.php';
+
+class Service_community_baptisms extends REST_Controller
+{
+    private $sub_module_id;
+
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+        $this->sub_module_id = 7;
+    }
+
+    public function index_get()
+    {               
+        $branch_id = (int)$this->get('branch_id'); 
+
+        $sub_module = $this->service_references_model->_get_sub_modules_by_id($branch_id, $this->sub_module_id);
+
+        $service_community_baptisms = [            
+            'sub_module' =>  $this->service_references_model->_get_sub_modules_by_id($branch_id, $this->sub_module_id),
+            'form_fields' => $this->service_references_model->_get_all_community_baptisms($branch_id)     
+        ];    
+
+        if (empty($branch_id)) {
+            // Invalid id, set the response and exit.
+            $this->response([
+                'status' => FALSE,
+                'message' => 'Bad Request'
+            ], REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
+        } else{
+            if ($sub_module === FALSE) {
+                $this->response([
+                    'status' => FALSE,
+                    'message' => 'Not Found'
+                ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+            }   
+            else {
+                if (empty($service_community_baptisms)) {
+                    // Set the response and exit
+                    $this->response([
+                        'status' => FALSE,
+                        'message' => 'Not Found'
+                    ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+                } else {
+                    // Set the response and exit
+                    $this->response($service_community_baptisms,REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+                }
+            }
+        } 
+        
+    }
+
+    
+}
