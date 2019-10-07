71080d2b7bd7454f1e87296fb8b78962fb5c2546
JOAN <joan.gregorio79@gmail.com>
2019-10-07 16:02:04 +0800

    updated controllers for service - confirmations, confraternities, events FMHH venues and marriages

---
+++ application/controllers/Service_confraternities.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+    Filename    : Service_confraternities.php
+    Location    : application/controllers/Service_confraternities.php
+    Purpose     : Service confraternities controller
+    Created     : 07/25/2019 19:05:13 by Scarlet Witch
+    Updated     : 10/07/2019 14:03:41 by Scarlet Witch
+    Changes     : added sub module id, branch_id
+*/
+
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+use Restserver\Libraries\REST_Controller;
+
+/** @noinspection PhpIncludeInspection */
+require APPPATH . 'libraries/REST_Controller.php';
+/** @noinspection PhpIncludeInspection */
+require APPPATH . 'libraries/Format.php';
+
+class Service_confraternities extends REST_Controller
+{
+    private $sub_module_id;
+
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+        $this->sub_module_id = 1;
+    }
+
+    public function index_get()
+    {               
+        $branch_id = (int)$this->get('branch_id'); 
+
+        $sub_module = $this->service_references_model->_get_sub_modules_by_id($branch_id, $this->sub_module_id);
+
+        $service_confraternities = [            
+            'sub_module' =>  $this->service_references_model->_get_sub_modules_by_id($branch_id, $this->sub_module_id),
+            'form_fields' => $this->service_references_model->_get_all_confraternities($branch_id)     
+        ];    
+
+        if (empty($branch_id)) {
+            // Invalid id, set the response and exit.
+            $this->response([
+                'status' => FALSE,
+                'message' => 'Bad Request'
+            ], REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
+        } else{
+            if ($sub_module === FALSE) {
+                $this->response([
+                    'status' => FALSE,
+                    'message' => 'Not Found'
+                ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+            }   
+            else {
+                if (empty($service_confraternities)) {
+                    // Set the response and exit
+                    $this->response([
+                        'status' => FALSE,
+                        'message' => 'Not Found'
+                    ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+                } else {
+                    // Set the response and exit
+                    $this->response($service_confraternities,REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+                }
+            }
+        } 
+        
+    }
+
+    
+}
